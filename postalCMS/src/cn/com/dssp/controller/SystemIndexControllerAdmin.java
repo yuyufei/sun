c5d9bafd5b65ef6580d1579c8dacd5bd9db71468
Administrator <Administrator@fishandfly>
2017-02-17 16:14:02 +0800

    解决一个无厘头问题

--- postalCMS/src/cn/com/dssp/controller/SystemIndexControllerAdmin.java
+++ postalCMS/src/cn/com/dssp/controller/SystemIndexControllerAdmin.java
@@ -5,22 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * 处理登录index的各种事件
- * <p>Title: SystemIndexControllerAdmin</p>
- * <p>Description: </p>
- * @author	fly
- * @date	2017年1月17日上午9:58:59
- * @version 1.0
- */
 @Controller
 @RequestMapping("/admin")
 public class SystemIndexControllerAdmin {
 	private static final Logger LOG=Logger.getLogger(SystemIndexControllerAdmin.class);
 	
-	/**
-	 * 登录，get方式
-	 */
 	@RequestMapping(value="login.html",method=RequestMethod.GET)
 	public String login(){
 		return "admin/login";
