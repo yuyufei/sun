82c439dae01639062eebfb028b416ecb7b0f4e06
fly <fly@fly>
2017-02-21 21:16:07 +0800

    晚上没写

--- postalCMS/src/cn/com/dssp/controller/SystemVerifyControllerAdmin.java
+++ postalCMS/src/cn/com/dssp/controller/SystemVerifyControllerAdmin.java
@@ -1,5 +1,23 @@
 package cn.com.dssp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 生成验证码
+ * @author fly
+ *
+ */
+@Controller
+@RequestMapping("/admin")
 public class SystemVerifyControllerAdmin {
+	@RequestMapping("/verifyCode")
+	public void verifyCode(HttpServletRequest request,HttpServletResponse response){
+		//设置浏览器不缓存本页
+		
+	}
 
 }
